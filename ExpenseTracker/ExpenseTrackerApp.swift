c0da590b06d96adc892477ada7a59e011dd75c9a
cesurkedi <cesurkedixx@protonmail.com>
2023-03-20 19:43:27 +0300

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Çare C. on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
