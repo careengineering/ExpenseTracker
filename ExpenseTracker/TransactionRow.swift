245eb70ad9889f066e1721d8a42093d7a9644425
cesurkedi <cesurkedixx@protonmail.com>
2023-03-20 20:50:50 +0300

    day1

--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct TransactionRow: View {
+    var transaction: Transaction
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            VStack(alignment: .leading, spacing:6) {
+                //MARK: Transaction Merchant
+                Text(transaction.merchant)
+                    .font(.subheadline)
+                    .bold()
+                    .lineLimit(1)
+               
+                //MARK: Transaction Category
+                Text(transaction.category)
+                    .font(.footnote)
+                    .opacity(0.7)
+                    .lineLimit(1)
+                
+                //MARK: Transaction Date
+                Text(transaction.dateParsed, format: .dateTime.year().month().day())
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+            
+            //MARK: Transaction Amount
+            Text(transaction.amount, format: .currency(code: "USD"))
+                .bold()
+                .foregroundColor(transaction.type == TransactionType.credit.rawValue ? Color.text : .primary)
+        }
+        .padding([.top, .bottom], 8)
+               
     }
 }
 
 struct TransactionRow_Previews: PreviewProvider {
     static var previews: some View {
-        TransactionRow()
+        TransactionRow(transaction: transactionPreviewData)
     }
 }
--- ExpenseTracker/myPersonalNotes.swift
+++ ExpenseTracker/myPersonalNotes.swift
@@ -5,4 +5,7 @@
 //  Created by Çare C. on 20.03.2023.
 //
 
+//Youtube  https://www.youtube.com/watch?v=Bu6fAlltatA
+// 23:22 kaldı
+
 import Foundation
