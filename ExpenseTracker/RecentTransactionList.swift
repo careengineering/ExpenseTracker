8a1e9726e2fb6c331f24e6700a88f40465cd6f61
cesurkedi <cesurkedixx@protonmail.com>
2023-03-21 22:01:41 +0300

    day2

---
+++ ExpenseTracker/RecentTransactionList.swift
@@ -0,0 +1,20 @@
+//
+//  RecentTransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Çare C. on 21.03.2023.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecentTransactionList_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentTransactionList()
+    }
+}
---
+++ ExpenseTracker/TransactionListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionListViewModel.swift
+//  ExpenseTracker
+//
+//  Created by Çare C. on 21.03.2023.
+//
+
+import Foundation
