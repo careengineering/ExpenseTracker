4d4d0d1c62ca19a3d0d4455faf2265e18a7293f9
cesurkedi <cesurkedixx@protonmail.com>
2023-03-23 21:52:42 +0300

    day3

---
+++ ExpenseTracker/TransactionList.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Çare C. on 23.03.2023.
+//
+
+import SwiftUI
+
+struct TransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionList_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionList()
+    }
+}
